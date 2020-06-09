5eaaebf9c304ce01e424d950a20fc49bd8c7f661
Lee Lin De <linde@itfactory.sg>
2020-06-09 21:29:47 +0800

    Added basic file structure.

---
+++ src/components/Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Screen = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+Screen.propTypes = {
+
+};
+
+export default Screen;
